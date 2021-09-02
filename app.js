a3cbb527316c64547f0990bd662e0b59208c1fe8
Atik Hasan <atikhasan801@gmail.com>
2021-09-02 16:10:34 +0600

    last commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const fetchData = async (searchKey) => {
   } catch (error) {
     spinnerShow("none");
     document.getElementById("search-result").innerHTML = `
-    <h3>No Data Found</h3>
+    <h3 class='text-light'>No Data Found</h3>
     `;
   }
 };
@@ -37,20 +37,24 @@ const showData = (bookList) => {
 
   if (bookList.docs.length === 0) {
     container.innerHTML = `
-      <h3>No Data Found</h3>
+      <h3 class='text-light'>No Data Found</h3>
       `;
   }
 
-  bookList.docs.slice(0, 30).forEach((book) => {
+  bookList.docs.slice(0, 32).forEach((book) => {
     const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    const alternateImg =
+      "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg";
     const div = document.createElement("div");
     div.innerHTML = `
-   
+ <div class='book-img'>   ${
+   book.cover_i ? ` <img src=${img}>` : `<img src=${alternateImg}>`
+ } </div>
     <p> Book Name: ${book.title}  </p>
     <p>  First Publish Date : ${book.first_publish_year}</p>
     <p> Publisher: ${book.publisher?.[0]} </p>
     <p> Author Name: ${book.author_name?.[0]} </p>
-    ${book.cover_i ? ` <img src=${img}>` : "<small> No Image </small>"}
+
     `;
 
     div.classList.add("book");
