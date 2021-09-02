2a8842b5002ed64a3b35461cf55e32d456a00ffc
Atik Hasan <atikhasan801@gmail.com>
2021-09-02 12:20:40 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// function for data fetch from api
+const fetchData = async (searchKey) => {
+  document.getElementById("total-book").textContent = "";
+  spinnerShow("block");
+  containerShow("none");
+  try {
+    const result = await fetch(
+      `https://openlibrary.org/search.json?q=${searchKey}`
+    );
+    const data = await result.json();
+
+    showData(data);
+  } catch (error) {
+    spinnerShow("none");
+    document.getElementById("search-result").innerHTML = `
+    <h3>No Data Found</h3>
+    `;
+  }
+};
+
+// function for receving search key word by clicking search button
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchKey = document.getElementById("search-field").value;
+  fetchData(searchKey);
+
+  document.getElementById("search-field").value = "";
+});
+
+// function for showing api data in ui
+const showData = (bookList) => {
+  document.getElementById(
+    "total-book"
+  ).innerText = `Total Book: ${bookList.numFound}`;
+
+  const container = document.getElementById("search-result");
+  container.textContent = "";
+
+  if (bookList.docs.length === 0) {
+    container.innerHTML = `
+      <h3>No Data Found</h3>
+      `;
+  }
+
+  bookList.docs.slice(0, 30).forEach((book) => {
+    const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    const div = document.createElement("div");
+    div.innerHTML = `
+   
+    <p> Book Name: ${book.title}  </p>
+    <p>  First Publish Date : ${book.first_publish_year}</p>
+    <p> Publisher: ${book.publisher?.[0]} </p>
+    <p> Author Name: ${book.author_name?.[0]} </p>
+    ${book.cover_i ? ` <img src=${img}>` : "<small> No Image </small>"}
+    `;
+
+    div.classList.add("book");
+    container.appendChild(div);
+  });
+  containerShow("block");
+  spinnerShow("none");
+};
+
+// show spinner function
+const spinnerShow = (display) => {
+  if (display === "none") {
+    document.getElementById("spinner").style.display = "none";
+  } else {
+    document.getElementById("spinner").style.display = "block";
+  }
+};
+
+// books container show or not function
+const containerShow = (display) => {
+  if (display === "none") {
+    document.getElementById("search-result").style.display = "none";
+  } else {
+    document.getElementById("search-result").style.display = "grid";
+  }
+};
